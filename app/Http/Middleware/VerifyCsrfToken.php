15cda9ee284190c82c8cebef091bcd792807ac0e
PoopEuy <mrakajanuarsyah@gmail.com>
2024-07-26 14:03:22 +0700

    update middleware for uploadImage

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'uploadImage/*', // Add your upload image route to this array
     ];
 }
