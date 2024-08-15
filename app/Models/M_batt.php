794af754082297712536fdb49032da6edb210fa1
PoopEuy <mrakajanuarsyah@gmail.com>
2024-08-15 08:54:49 +0700

    create new migration, update mbatts models

--- app/Models/M_batt.php
+++ app/Models/M_batt.php
@@ -11,6 +11,6 @@ class M_batt extends Model
     protected $table = "m_batts";
     protected $primaryKey = "id";
     protected $fillable = [
-       'cell_sern', 'capa_grad', 'crtn_sern', 'm', 'c_po', 'v_po', 'ir_po', 'k', 'w', 'ha', 'hc', 't', 'bin', 'v_gr', 'delta_mv', 'v_status', 'ir_gr', 'delta_ir', 'ir_status', 'frame_sn', 'cell', 'd_test', 'batch', 'po'
+       'cell_sern', 'capa_grad', 'crtn_sern', 'm', 'c_po', 'v_po', 'ir_po', 'k', 'w', 'ha', 'hc', 't', 'bin', 'v_gr', 'delta_mv', 'v_status', 'ir_gr', 'delta_ir', 'ir_status', 'frame_sn', 'cell', 'd_test', 'batch', 'po', 'notes'
     ];
 }
